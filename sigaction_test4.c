8f4014024db6b62d610a36a01bd7a093723a4363
soonhong22 <tnsghd55@naver.com>
2018-10-22 00:47:56 +0900

    middletest

---
+++ sigaction_test4.c
@@ -0,0 +1,41 @@
+/*igaction() 함수의 세 번째 인수를 이용하여*/
+/*sigaction함수를 호출하기 전에 설정된 상황으로 되돌리는 프로그램*/
+
+
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void signalHandler(int signo);
+int signal_count=0;
+
+main()
+{
+	struct sigaction new_act;
+	struct sigaction old_act;
+	new_act.sa_handler = signalHandler;
+
+	/* new_act.sa_mask는 시그널을 처리하는 동안 블록시킬 시그널을 모아 놓은
+	   시그널 집합인데, 시그널 집합을 비웠으므로 모든 시그널은 블록되지 않음 */
+	sigemptyset(&new_act.sa_mask);
+	new_act.sa_flags = 0;
+
+
+	/* SIGINT에 대해 new_act로 설정하는데, 호출하기 전 행동 정보가 old_act에 저장 */
+	sigaction(SIGINT, &new_act, &old_act);
+
+	while(1) {
+		printf("waiting signal\n");
+		sleep(1);
+		if (signal_count > 3){
+			/* SIGINT에 대해 old_act(예전 행동)로 설정 */
+			sigaction(SIGUSR1 &old_act, NULL);
+		}
+	}
+}
+void signalHandler(int signo)
+{
+	signal_count++;
+	printf("Hi! signal %d\n", signal_count);
+}
